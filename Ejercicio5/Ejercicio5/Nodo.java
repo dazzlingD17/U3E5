335cdfa590efdf9726feb1ca7bd37af7a4503b6e
dazzling diaz <71990206+dazzlingD17@users.noreply.github.com>
2020-12-02 16:31:46 -0600

    Create Nodo.java

---
+++ Ejercicio5/Ejercicio5/Nodo.java
@@ -0,0 +1,12 @@
+package Ejercicio5;
+
+public class Nodo {
+     int elemento;
+    Nodo Siguiente;
+    
+    
+    public Nodo (int elemento){
+        this.elemento=elemento;
+        Siguiente=null; 
+}
+}
